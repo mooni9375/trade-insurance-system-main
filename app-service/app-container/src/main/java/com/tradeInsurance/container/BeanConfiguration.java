5c95945f8e9a936bf9f40af62a47ba09c4132d65
mooni9375 <moonmoon9375@gmail.com>
2024-12-20 23:23:38 +0900

    kafka producer setted commit.

--- app-service/app-container/src/main/java/com/tradeInsurance/container/BeanConfiguration.java
+++ app-service/app-container/src/main/java/com/tradeInsurance/container/BeanConfiguration.java
@@ -2,6 +2,8 @@ package com.tradeInsurance.container;
 
 import com.tradeInsurance.domain.AppDomainService;
 import com.tradeInsurance.domain.AppDomainServiceImpl;
+import com.tradeInsurance.domain.ports.output.message.publisher.review.ReviewRequestMessagePublisher;
+import com.tradeInsurance.messaging.publisher.kafka.AppReviewEventKafkaPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,4 @@ public class BeanConfiguration {
     public AppDomainService appDomainService() {
         return new AppDomainServiceImpl();
     }
-
 }
--- app-service/app-messaging/src/main/java/com/tradeInsurance/messaging/publisher/kafka/AppReviewEventKafkaPublisher.java
+++ app-service/app-messaging/src/main/java/com/tradeInsurance/messaging/publisher/kafka/AppReviewEventKafkaPublisher.java
@@ -1,6 +1,5 @@
 package com.tradeInsurance.messaging.publisher.kafka;
 
-import com.tradeInsurance.domain.config.AppServiceConfigData;
 import com.tradeInsurance.domain.dto.message.AppReviewMessage;
 import com.tradeInsurance.domain.ports.output.message.publisher.review.ReviewRequestMessagePublisher;
 import com.tradeInsurance.messaging.kafka.model.ReviewRequestModel;
@@ -18,15 +17,12 @@ import org.springframework.stereotype.Component;
 public class AppReviewEventKafkaPublisher implements ReviewRequestMessagePublisher {
 
     private final KafkaProducer<String, ReviewRequestModel> kafkaProducer;
-    private final AppServiceConfigData appServiceConfigData;
     private final AppMessagingDataMapper appMessagingDataMapper;
 
     @Autowired
     public AppReviewEventKafkaPublisher(KafkaProducer<String, ReviewRequestModel> kafkaProducer,
-                                        AppServiceConfigData appServiceConfigData,
                                         AppMessagingDataMapper appMessagingDataMapper) {
         this.kafkaProducer = kafkaProducer;
-        this.appServiceConfigData = appServiceConfigData;
         this.appMessagingDataMapper = appMessagingDataMapper;
     }
 
@@ -36,9 +32,9 @@ public class AppReviewEventKafkaPublisher implements ReviewRequestMessagePublish
         ReviewRequestModel reviewRequestModel = appMessagingDataMapper.appReviewMessageToReviewRequestModel(appReviewMessage);
 
         try {
-            kafkaProducer.send(appServiceConfigData.getReviewRequestTopicName(), reviewRequestModel);
-
+            kafkaProducer.send("review-request-topic", reviewRequestModel);
             log.info("ReviewRequestModel sent to Kafka for order id: {}", reviewRequestModel.getAppId());
+
         } catch (Exception e) {
             log.error("Error while sending ReviewRequestModel " +
                             "to kafka with order id: {} and  error: {}", reviewRequestModel.getAppId(), e.getMessage());
