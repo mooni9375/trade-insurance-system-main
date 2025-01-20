fd15be5c7cb96a8326688a556c0bca7132efe342
mooni9375 <moonmoon9375@gmail.com>
2025-01-21 08:38:02 +0900

    transaction analysis commit

--- app-service/app-domain/app-application-service/src/main/java/com/tradeInsurance/domain/AppCreateCommandHandler.java
+++ app-service/app-domain/app-application-service/src/main/java/com/tradeInsurance/domain/AppCreateCommandHandler.java
@@ -33,6 +33,13 @@ public class AppCreateCommandHandler {
      *  Transaction processing : Processed by Helper
      *  Event publishing : Published by Handler
      */
+    /**
+     * 트랜잭션 처리에 대한 고민
+     *  현재 문제 : 단일 서비스 내 트랜잭션에만 의존 -> 데이터 일관성과 원자성 보장 필요.
+     *
+     *  청약 트랜잭션 처리 후 단계인 이벤트 발행, 메시지 송수신, 심사 서비스 단계에서 오류 발생시, 청약과 심사의 데이터 일관성이 보장되지 않음.
+     *  따라서 추후 Outbox Pattern 혹은 SAGA Pattern 도입함으로써 연관된 두 마이크로 서비스가 같은 트랜잭션 컨텍스트 내에서 처리되도록 조치 필요.
+     */
     public CreateAppResponse createApp(CreateAppCommand createAppCommand) {
 
         // Transaction processing
