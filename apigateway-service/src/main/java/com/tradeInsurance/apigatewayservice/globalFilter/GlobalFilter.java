a94106a19c3c3d35ee1dddbe4dcc6dc34b4775dd
mooni9375 <moonmoon9375@gmail.com>
2025-01-21 07:53:31 +0900

    config changed commit

--- apigateway-service/src/main/java/com/tradeInsurance/apigatewayservice/globalFilter/GlobalFilter.java
+++ apigateway-service/src/main/java/com/tradeInsurance/apigatewayservice/globalFilter/GlobalFilter.java
@@ -17,21 +17,39 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
         super(Config.class);
     }
 
+    /**
+     * GatewayFilterFactory Interface 구현
+     *  Gateway 요청(Request) 및 응답(Response)을 처리하기 위한 사전(pre) 및 사후(post) 로직을 정의.
+     *
+     * Config
+     *  application.yml 파일의 설정을 읽어들여 파라미터로 전달
+     */
     @Override
     public GatewayFilter apply(Config config) {
 
         // GatewayFilter 구현 클래스 없이 람다로 간결화
+        /**
+         * ServerWebExchange
+         *  클라이언트와 서버 간 데이터 교환의 중심 역할
+         *  요청과 응답을 GatewayFilterChain에 전달하여 각 필터가 처리할 수 있도록 함
+         *  요청/응답과 관련된 임시 데이터를 저장하고 공유할 수 있는 컨텍스트 역할
+         */
+        //  :  .
         return ((exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpRequest  request  = exchange.getRequest(); // 현재 요청과 응답
             ServerHttpResponse response = exchange.getResponse();
 
             log.info("Global Filter baseMessage: {}", config.getBaseMessage());
 
+            // 전처리 작업
             if (config.isPreLogger()) {
                 log.info("Global Filter Start: request id -> {}", request.getId());
             }
 
+            // 최상위 람다의 반환값
+            // 체인 필터 실행 : 현재 필터 이후 다음 필터(후처리 작업)를 비동기(Runnable)로 실행
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                // 후처리 작업
                 if (config.isPostLogger()) {
                     log.info("Global Filter End: response code id -> {}", response.getStatusCode());
                 }
